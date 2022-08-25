dc4602bf127abc3d84a774a5bc9c5816879d7d67
Akash Kamble <akashkamble012@gmail.com>
2022-08-25 07:59:54 +0530

    fix: date formatt changed

--- lib/common/utils/date_time_utils.dart
+++ lib/common/utils/date_time_utils.dart
@@ -2,7 +2,7 @@ import 'package:intl/intl.dart';
 
 class DateTimeUtils {
   static String formatDate(DateTime dateTime) {
-    return DateFormat('yyyy/MM/dd').format(dateTime);
+    return DateFormat('yyyy-MM-dd').format(dateTime);
     ;
   }
 }
